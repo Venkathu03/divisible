497bdbd9c9e04288e12f2b3cd1294dc53020c4c4
Vengadesh P <104545809+Venkathu03@users.noreply.github.com>
2022-07-18 22:20:03 +0530

    Add files via upload

---
+++ divisible/divisible.js
@@ -0,0 +1,13 @@
+//divisible 5
+
+let x3=prompt('enter the value');
+console.log('value:',x3);
+if(x3==0){
+    console.log('zero');
+}
+else if(x3%5==0){
+    console.log('divisible');
+}
+else{
+    console.log('not divisible');
+}
